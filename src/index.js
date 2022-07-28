dfed21da83a7e1392b9afaa773a16e97d6f16ec7
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-28 09:25:32 -0400

    core deliverables fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 // Your code here
+const url = 'http://localhost:3000/characters';
+const characterBar = document.querySelector('#character-bar');
+const nameDetail = document.querySelector('#name');
+const imageDetail = document.querySelector('#image');
+const voteDetail = document.querySelector('#vote-count');
+const voteForm = document.querySelector('#votes-form');
+const resetBtn = document.querySelector('#reset-btn');
+
+const populateBar = function(characters){
+    characters.forEach((el, i) => {
+        let newSpan = document.createElement('span');
+        newSpan.innerText = el.name;
+        newSpan.addEventListener('click', function(e){
+            showCharacter(el);
+        })
+        characterBar.append(newSpan);
+    })
+}
+
+const showCharacter = function(character){
+    nameDetail.innerText = character.name;
+    imageDetail.src = character.image;
+    voteDetail.innerText = character.votes;
+}
+
+const onFormSubmit = function(){
+    let newVotes = parseInt(e.target.votes.value);
+    let curVotes = parseInt(voteDetail.innerText);
+    voteDetail.innerText = newVotes + curVotes;
+}
+
+
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    populateBar(data);
+})
+
+voteForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    onFormSubmit(); 
+})
