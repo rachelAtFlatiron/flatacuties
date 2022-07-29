21125a96eaf5fe499fd7d6d6035032e4dece06b5
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-29 09:43:53 -0400

    attempt at post patch

--- src/index.js
+++ src/index.js
@@ -19,7 +19,13 @@ const voteDetail = document.querySelector('#vote-count');
 
 //vote form and reset button
 const voteForm = document.querySelector('#votes-form');
+const newForm = document.querySelector('#character-form')
 const resetBtn = document.querySelector('#reset-btn');
+
+//global current character - safer in javascript - HTML is easily tampered with
+//but still I hate global variables
+//used for PATCH and DELETE and POST 
+let curChar;
 
 /*
 *
@@ -48,21 +54,71 @@ const getCharacters = async function(){
 //takes characters and creates span at top of page
 const populateBar = function(characters){
     characters.forEach((el, i) => {
-        let newSpan = document.createElement('span');
-        newSpan.innerText = el.name;
-        //on span click invoke showCharacter to populate info in middle of page
-        newSpan.addEventListener('click', function(e){
-            showCharacter(el);
-        })
-        characterBar.append(newSpan);
+        addCharacter(el);
     })
+}
+
+//adds a character to the span menu bar
+const addCharacter = function(character) {
+    let newSpan = document.createElement('span');
+    newSpan.innerText = character.name;
+    //on span click invoke showCharacter to populate info in middle of page
+    newSpan.addEventListener('click', function(e){
+        showCharacter(el);
+    })
+    characterBar.append(newSpan);
 }
 
 //takes one character and shows info in middle of page
 const showCharacter = function(character){
+    curChar = character; //I hate global variables, set global curChar
     nameDetail.innerText = character.name;
     imageDetail.src = character.image;
     voteDetail.innerText = character.votes;
+}
+
+
+
+//updates votes for global curChar
+const updateVotes = async function(e) {
+    let newVotes = parseInt(e.target.votes.value);
+    let curVotes = curChar.votes;
+    
+    try {
+        let res = await fetch(`${url}/${curChar.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({votes: curVotes + newVotes}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        let data = await res.json();
+        voteDetail.innerText = data.votes;
+
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//resets votes for global curChar
+const resetVotes = async function() {
+    try {
+        //patch request
+        let res = await fetch(`${url}/${curChar.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({votes: 0}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        let data = await res.json();
+        //on success update votes with 0
+        voteDetail.innerText = data.votes;
+
+    } catch(err) {
+        console.log(err);
+    }
+
 }
 
 //when votes form is submitted add new vote count to current vote
@@ -76,6 +132,28 @@ const onFormSubmit = function(e){
     //reset form
     e.target.reset();
 }
+
+const onNewCharacter = async function(e){
+    try {
+        let res = await fetch(`${url}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name: e.target.value.name,
+                votes: 0,
+                image: e.target['image-url'].value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        let data = await res.json();
+        addCharacter(data);
+        showCharacter(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 
 /*
 *
@@ -94,5 +172,13 @@ console.log(`outside of async/await function getCharacters is ${getCharacters()}
 voteForm.addEventListener('submit', function(e){
     e.preventDefault();
     //invoke onFormSubmit function
-    onFormSubmit(e); 
+    updatesVotes(e); 
+})
+
+resetBtn.addEventListener('click', function(){
+    resetVotes();
+})
+
+newForm.addEventListener('submit', function(e){
+    onNewCharacter(e);
 })
